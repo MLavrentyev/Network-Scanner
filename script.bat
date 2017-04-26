9ab0aa6276e73ad28537a82cebddf49121015121
Mark Lavrentyev <lavrema@outlook.com>
2017-04-26 19:40:51 -0400

    Initial commit

---
+++ script.bat
@@ -0,0 +1,12 @@
+For /f "tokens=1-2" %%a in ('date /t') do (set newDate_=%%b)
+For /f "tokens=1-3 delims=/" %%a in ('echo %newDate_%') do (set date_=%%a-%%b-%%c)
+
+For /f "tokens=1-2 delims=." %%a in ('echo %TIME%') do (set otherTime_=%%a)
+For /f "tokens=1-3 delims=:" %%a in ('echo %otherTime_%') do (set time_=%%a-%%b-%%c)
+
+md %date_%
+cd %date_%
+
+nmap -sP 10.0.0.1/32 > newText.txt
+
+pause
