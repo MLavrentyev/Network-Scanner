c09c1d7caf3e4182e1f1b2ad808fadacb2fdb954
Mark Lavrentyev <lavrema@outlook.com>
2017-05-01 16:36:00 -0400

    Graph all the values on given dates on one graph

--- data_extractor.py
+++ data_extractor.py
@@ -2,42 +2,50 @@ import os, glob
 import datetime as dt
 import matplotlib.pyplot as plt
 
-#folder = input("Enter date to retrieve (mm-dd-yyyy): ")
-folder = "04-27-2017"
+#folders = input("Enter date to retrieve (mm-dd-yyyy): ")
+folders = ["04-27-2017","04-28-2017","05-01-2017"]
 
-date = [int(x.strip()) for x in folder.split("-")]
-epoch = dt.datetime.utcfromtimestamp(0)
-morn = dt.datetime(date[2], date[0], date[1])
 
 times = []
 vals = []
 
-for filename in os.listdir(folder):
-    if not(filename.endswith(".txt")):
-        continue
+for folder in folders:
 
-    #Extract time from filename
-    filename = filename[:-4]
-    timePieces = [int(x.strip()) for x in filename.split("-")]
-    time = dt.datetime(hour=timePieces[0], minute=timePieces[1], \
-                   second=timePieces[2], year=date[2], month=date[0], \
-                       day=date[1])
-
-    #Extract number of hosts up from file content
-
-    with open(folder + "/" + filename + ".txt", "r") as file:
-        data = file.readlines()
-
-        try:
-            num = int([(x.strip())[1:] for x in data[len(data)-1].split()][5])
-        except ValueError:
+    date = [int(x.strip()) for x in folder.split("-")]
+    morn = dt.datetime(date[2], date[0], date[1])
+    tim = []
+    val = []
+    for filename in os.listdir(folder):
+        if not(filename.endswith(".txt")):
             continue
 
-    times.append((time-morn).total_seconds()/3600)
-    vals.append(num)
+        #Extract time from filename
+        filename = filename[:-4]
+        timePieces = [int(x.strip()) for x in filename.split("-")]
+        time = dt.datetime(hour=timePieces[0], minute=timePieces[1], \
+                       second=timePieces[2], year=date[2], month=date[0], \
+                           day=date[1])
 
-times, vals = zip(*sorted(zip(times, vals)))
+        #Extract number of hosts up from file content
 
-plt.scatter(times, vals)
+        with open(folder + "/" + filename + ".txt", "r") as file:
+            data = file.readlines()
+
+            try:
+                num = int([(x.strip())[1:] for x in data[len(data)-1].split()][5])
+            except ValueError:
+                continue
+
+        tim.append((time-morn).total_seconds()/3600)
+        val.append(num)
+    # End looping over files
+
+    times.append(tim)
+    vals.append(val)
+# End looping over folders
+
+#Graph the actual values
+for i in range(len(times)):
+    plt.scatter(times[i], vals[i])
 plt.ylim(ymax=700, ymin=0)
 plt.show()
